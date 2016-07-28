663ba3bbe67a737b6355aac743cd8a4b61a61efe
= <eda@mac-mini-4.local>
2016-07-29 10:10:58 +1200

    Created Server file

---
+++ server.js
@@ -0,0 +1,25 @@
+var fs = require('fs')
+var path = require('path')
+var data = require('./data')
+var express = require('express')
+var hbs = require('express-handlebars')
+
+var app = express()
+
+app.engine('hbs', hbs())
+app.set('view engine', 'hbs')
+app.set('views', path.join(__dirname, 'views'))
+
+app.get("/", function (req, res) {
+
+})
+
+app.get("/page", function(req, res) {
+  res.render('index', data);
+})
+
+
+var PORT = process.env.PORT || 3000
+app.listen( PORT, function () {
+ console.log('The server is running on port: ', PORT)
+})
